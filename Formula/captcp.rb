2f8dd3c36380bba6097c599a8543906f469e5215
Scott Sanders <jssjr@github.com>
2016-07-21 18:16:23 -0400

    up

--- Formula/captcp.rb
+++ Formula/captcp.rb
@@ -5,7 +5,9 @@ class Captcp < Formula
   sha256 "c7f61a15aba9aefa517ee3ebbd92d5ca9fd5abc4badad8fef99c4d37a95d13a3"
 
   def install
+    inreplace "captcp.py", "#!/usr/bin/env python2", "#!/usr/bin/env python"
     system "make", "PREFIX=#{prefix}"
     system "make", "install", "PREFIX=#{prefix}"
+
   end
 end
