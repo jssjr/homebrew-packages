ad51f15be11e633d143ffa9c7d87a9b668e13939
Scott Sanders <jssjr@github.com>
2016-11-21 10:40:47 -0500

    Fix the cask

--- Casks/hackable-slack-client.rb
+++ Casks/hackable-slack-client.rb
@@ -1,4 +1,4 @@
-cask "HackableSlackClient" do
+cask "hackable-slack-client" do
   name "Hackable Slack Client"
   version "0.3.2"
   sha256 "44cfdcb508899133ab17fc86b54ce378534cc301dd2dcbf714efdd00c48c2f04"
