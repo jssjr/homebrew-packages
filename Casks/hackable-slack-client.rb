9e046676ef94a232cc4fb5d22439ddcce0ec2a40
Scott Sanders <jssjr@github.com>
2016-11-21 10:37:01 -0500

    Create cask for Hackable Slack Client

---
+++ Casks/hackable-slack-client.rb
@@ -0,0 +1,11 @@
+cask HackableSlackClient do
+  name "Hackable Slack Client"
+  version "0.3.2"
+  sha256 "44cfdcb508899133ab17fc86b54ce378534cc301dd2dcbf714efdd00c48c2f04"
+  url "https://dinosaur.s3.amazonaws.com/hackable-slack-client-#{version}.zip"
+
+  desc "Inject CSS or JS into Slack, per-team."
+  homepage "https://github.com/bhuga/hackable-slack-client"
+
+  app "#{name}.app"
+end
