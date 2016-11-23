c9eed29ffadcefc0c4179e06d200e2b63af83c5a
Scott Sanders <jssjr@github.com>
2016-11-23 09:52:39 -0500

    DJI DNG Cleaner cask

---
+++ Casks/dji-dng-cleaner.rb
@@ -0,0 +1,10 @@
+cask "dji-dng-cleaner" do
+  name "DJI DNG Cleaner"
+  version "1.1"
+  url "http://dl.djicdn.com/downloads/inspire_1/DJI_DNG_Cleaner_V#{version}.dmg"
+  sha256 "d27e1110e7b4945faa717b3c7b0dd658ecc43650cc786aad4f6ce279624d9fac"
+
+  homepage "https://www.dji.com"
+
+  app "DJI DNG Cleaner v#{version}.app"
+end
