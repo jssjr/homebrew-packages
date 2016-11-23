7b5efd165c73611cd9f116f51cde8c7bcd0560ca
Scott Sanders <jssjr@github.com>
2016-11-23 09:47:35 -0500

    DJI Assistant 2 Cask

---
+++ Casks/dji-assistant2.rb
@@ -0,0 +1,10 @@
+cask "dji-assistant2" do
+  name "DJI Assistant 2"
+  version "1.0.7"
+  url "https://dl.djicdn.com/downloads/dji_assistant/20161115/DJI_Assistant_2_#{version}.dmg"
+  sha256 "65b986cef3653a75342adc90ddaba74ca01022aced3917c5493d936743023145"
+
+  homepage "https://www.dji.com"
+
+  app "Assistant.app"
+end
